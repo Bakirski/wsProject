d565224109664ed4c3d8531914ab963ed7649229
Bakir Mandžukić <bakir.mandzukic@outlook.com>
2025-05-17 20:11:24 +0200

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import WebSocket from "ws";
+
+const app = express();
+const port = 4000;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(express.static("public"));
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Back and Front successfully connected." });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
